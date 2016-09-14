16ff66e60331685580be2788d99ba81fc9c452b5
Jimmy Tran <jtran064@ucr.edu>
2016-09-14 12:57:25 -0700

    basic logging

--- selfshow/__main__.py
+++ selfshow/__main__.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import logging
 from PyQt4.Qt import QApplication, QDialog, QWidget, QMainWindow
 from mainwindow import Ui_MainWindow
 import selfspy.stats as stats
@@ -7,14 +8,18 @@ import selfspy.stats as stats
 DB_DIR = ".selfspy"
 DB_NAME = "selfspy.sqlite"
 
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
+        level=logging.DEBUG)
+log = logging.getLogger(__name__)
+
 app = QApplication(sys.argv)
 window = QMainWindow()
 ui = Ui_MainWindow()
 ui.setupUi(window)
 
 db_path = os.path.join(os.path.expanduser("~"), DB_DIR, DB_NAME)
-print db_path
 if(os.path.exists(db_path)):
+    log.info("DB file: {}".format(db_path))
     ss = stats.Selfstats(db_path, 
             {'key_freqs': False, 'process': None, 'back': None, 'date': None, 'periods':
                 None, 'showtext': False, 'id': None, 'password': None, 'data_dir':
